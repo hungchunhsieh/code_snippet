90478294b475f63d011fb1cc2e1c1dd7b2da15d8
Hung-Chun Hsieh <hungchun.hsieh@gmail.com>
2024-07-30 16:06:26 +0800

    Update ios_app.m

--- ios_app.m
+++ ios_app.m
@@ -58,15 +58,3 @@
     }
     return NO;
 }
-
-(* https://stackoverflow.com/questions/65580496/sanitize-nsurl-in-objective-c *)
-
-- (BOOL)application:(UIApplication *)application
-   openURL2:(NSURL *)url
-   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-
-NSString *urlEncoded = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-
-  return [RCTLinkingManager application:application openURL:urlEncoded options:options];
-} 
